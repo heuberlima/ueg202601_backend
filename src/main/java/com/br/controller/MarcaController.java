b093b6b5d995382d76fcc50bd1d91b1769e928b8
heuber.lima <heuber.lima@gmail.com>
2026-03-24 20:11:09 -0300

    Inclusão dos métodos alterar e excluir

--- src/main/java/com/br/controller/MarcaController.java
+++ src/main/java/com/br/controller/MarcaController.java
@@ -21,46 +21,77 @@ import com.br.exception.ResourceNotFoundException;
 import com.br.model.Marca;
 import com.br.repository.MarcaRepository;
 
-
 @RequestMapping("/cmarca/")
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 public class MarcaController {
-	
-	
-	//Cria o repositório JPA de forma automática e autogerenciado
+
+	// Cria o repositório JPA de forma automática e autogerenciado
 	@Autowired
 	private MarcaRepository mrep;
-	
-	
-	//Método listar - trazer todas as marcas do banco
+
+	// Método listar - trazer todas as marcas do banco
 	@GetMapping("/marca")
-	public List<Marca> listar(){
-		
+	public List<Marca> listar() {
+
 		return this.mrep.findAll(Sort.by(Sort.Direction.DESC, "codigo"));
-		
+
 	}
-	
-	//Método consultar - trazer uma marca, caso exista, pelo codigo
+
+	// Método consultar - trazer uma marca, caso exista, pelo codigo
 	@GetMapping("/marca/{id}")
 	public ResponseEntity<Marca> consultar(@PathVariable Long id) {
-		
-		Marca marca = this.mrep.findById(id).orElseThrow(()->
-		new ResourceNotFoundException("Marca nao encontrada: " + id));
-		
+
+		Marca marca = this.mrep.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Marca nao encontrada: " + id));
+
 		return ResponseEntity.ok(marca);
-		
+
 	}
-	
-	
-	//Método inserir - insere uma marca
+
+	// Método inserir - insere uma marca
 	@PostMapping("/marca")
 	public Marca inserir(@RequestBody Marca marca) {
-		
+
 		return this.mrep.save(marca);
+
+	}
+
+	// Método alterar - altera uma marca que foi passada como parâmetro
+	@PutMapping("/marca/{id}")
+	public ResponseEntity<Marca> alterar(@PathVariable Long id, @RequestBody Marca marca) {
+
+		// Consulta a marca, para verificar se ela existe no banco
+		Marca marcaConsultado = this.mrep.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Marca nao encontrada: " + id));
+
+		// Atualiza a marca consultada com os dados da marca que foi passada como
+		// parâmetro
+		marcaConsultado.setCodigo(marca.getCodigo());
+		marcaConsultado.setNome(marca.getNome());
+
+		// Salva as alterações
+		Marca marcaAtualizado = this.mrep.save(marcaConsultado);
+
+		// Retorna a marca com as atualizações efetivadas
+		return ResponseEntity.ok(marcaAtualizado);
+
+	}
+
+	@DeleteMapping("/marca/{id}")
+	public ResponseEntity<Map<String, Boolean>> excluir(@PathVariable Long id) {
+		
+		// Consulta a marca, para verificar se ela existe no banco
+		Marca marcaConsultado = this.mrep.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Marca nao encontrada: " + id));
+		
+		this.mrep.delete(marcaConsultado);
+		
+		Map<String, Boolean> resposta = new HashMap<>();
+		resposta.put("Marca excluida", true);
+		
+		return ResponseEntity.ok(resposta);
 		
 	}
-	
-	
 
 }
